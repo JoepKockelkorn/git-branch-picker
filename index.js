25163e3c7273c493f0e00e75326900c006e75508
Joep Kockelkorn <joepkockelkorn@gmail.com>
2019-11-24 12:59:14 +0100

    feat: 🎸 implement using isomorphic-git

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 (async () => {
-  // const inquirer = require("inquirer");
+  const inquirer = require('inquirer');
   const fs = require('fs');
-  const os = require('os');
-  const path = require('path');
   const git = require('isomorphic-git');
-  const branches = await git.listBranches({ dir: './', fs });
+  git.plugins.set('fs', fs);
+
+  const dir = './';
+  const currentBranch = await git.currentBranch({ dir });
+  const branches = await git.listBranches({ dir });
+  const { branch } = await inquirer.prompt({
+    name: 'branch',
+    message: 'Which branch would you like to checkout?',
+    choices: [...branches],
+    type: 'list',
+  });
+  if (branch === currentBranch) {
+    console.log(`Branch '${branch}' is already checked out.`);
+  } else {
+    console.log(`Checking out ${branch}`);
+  }
+  await git.checkout({ dir, ref: branch });
 })().catch(e => console.error(e.message));
