bc4a1f1c0618af50613d31410e385295425e2432
Joep Kockelkorn <joepkockelkorn@gmail.com>
2019-11-24 13:19:19 +0100

    feat: 🎸 add logging coloring

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
   const { current, all: branches } = await simpleGit.branchLocal();
   const filteredBranches = branches.filter(b => b !== current);
   if (filteredBranches.length === 0) {
-    console.log('Your current branch is the only branch.');
+    log('Your current branch is the only branch.');
     return;
   }
   const { branch } = await inquirer.prompt({
@@ -15,7 +15,22 @@
     choices: [...filteredBranches],
     type: 'list',
   });
-  console.log(`Checking out ${branch}`);
   await simpleGit.checkout(branch);
-  console.log(`Successfully checked out ${branch}`);
-})().catch(e => console.error(e.message));
+  logSuccess(`Successfully checked out ${branch}`);
+})().catch(e => {
+  const message = e.message || 'Oops, something went wrong...';
+  logError(message);
+});
+
+const chalk = require('chalk');
+function logSuccess(message) {
+  console.log(chalk.green(message));
+}
+
+function log(message) {
+  console.log(message);
+}
+
+function logError(message) {
+  console.error(chalk.red(message));
+}
